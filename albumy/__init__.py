b4172c8b4e1c6a9a7be682ba37e133648f5f1fea
xiaohutuxian111 <2398391057@qq.com>
2023-05-14 11:22:13 +0800

    写入角色权限

--- albumy/__init__.py
+++ albumy/__init__.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-    :author: Grey Li (李辉)
-    :url: http://greyli.com
-    :copyright: © 2018 Grey Li <withlihui@gmail.com>
-    :license: MIT, see LICENSE for more details.
-"""
+
 import os
 
 import click
@@ -24,7 +19,6 @@ def create_app(config_name=None):
         config_name = os.getenv('FLASK_CONFIG', 'development')
 
     app = Flask('albumy')
-
     app.config.from_object(config[config_name])
 
     register_extensions(app)
